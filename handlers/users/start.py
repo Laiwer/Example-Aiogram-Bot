2388ec00c813daeb7d18746ef34ada7505bf7f8f
Laiwer <87445634+Laiwer@users.noreply.github.com>
2023-02-27 23:18:36 +0300

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,7 +1,6 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 from keyboards.default.mainKeyboard import mainKeyboard
-from dataBase.base import existe_user_in_data_base, add_user_in_data_base
 from loader import dp
 from aiogram.utils.markdown import text
 
