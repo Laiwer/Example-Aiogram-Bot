ab10f707d6c26bf3a6910810ca9deba681da2d84
Laiwer <87445634+Laiwer@users.noreply.github.com>
2023-02-27 23:19:09 +0300

    v0.12

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,7 +2,6 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 from keyboards.default.mainKeyboard import mainKeyboard
 from loader import dp
-from aiogram.utils.markdown import text
 
 
 @dp.message_handler(CommandStart())
